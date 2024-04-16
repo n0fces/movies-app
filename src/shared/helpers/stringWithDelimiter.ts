788ba5f3f49eeaa49318759481d7c693ed0510ef
n0fces <ilyaabzalov@gmail.com>
2024-04-16 19:03:47 +0300

    refactor: add conditional for zero array in stringWithDelimiter

--- src/shared/helpers/stringWithDelimiter.ts
+++ src/shared/helpers/stringWithDelimiter.ts
@@ -2,7 +2,7 @@ export const stringWithDelimiter = <T = 'string'>(
 	delimiter: string,
 	arr: Array<T | null | undefined> | null | undefined
 ) => {
-	if (Array.isArray(arr)) {
+	if (Array.isArray(arr) && arr.length) {
 		return arr.filter(Boolean).join(delimiter);
 	}
 };
